410b4bf48e0310ba7ff49d7bb121afea467cc1fb
Ponce <ponceluis974@gmail.com>
2022-02-01 10:27:42 -0800

    first commit

---
+++ script.js
@@ -0,0 +1,57 @@
+const container = document.querySelector('#container');
+container.style.cssText += 'height: 832px; width: 832px;';
+let gridwidth = 16;
+let gridheight = 16;
+let boxes = gridheight * gridwidth;
+const blackButton = document.querySelector('#black');
+const rgbButton = document.querySelector('#rgb');
+const resetButton = document.querySelector('#reset');
+
+
+
+for (let index = 0; index < boxes; index++) {
+   let div = document.createElement('div')
+   div.classList.add('inside');
+   div.addEventListener('mouseover', function(event){
+    event.target.style.backgroundColor = 'black';
+})
+   container.appendChild(div)
+}
+
+
+
+
+function randomColor(){
+    let letters = '0123456789ABCDEF';
+    let color = '#';
+    for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+      }
+      return color;
+}
+
+blackButton.addEventListener('click', function(){
+    let cell = container.children;
+    for (let i = 0; i < boxes; i++) {
+        cell[i].addEventListener('mouseover', function(event){
+            event.target.style.backgroundColor = 'black';
+        })
+    }
+})
+
+rgbButton.addEventListener('click', function(){
+    let cell = container.children;
+    for (let i = 0; i < boxes; i++) {
+        cell[i].addEventListener('mouseover', function(event){
+            event.target.style.backgroundColor = randomColor();
+        })
+    }
+})
+resetButton.addEventListener('click', function(){
+    let cell = container.children;
+    for (let i = 0; i < boxes; i++) {
+        cell[i].style.backgroundColor = 'white';
+
+    }
+})
+
